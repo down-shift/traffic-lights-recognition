53558f78e70b08bbde109366ee95fe3450221e43
down-shift <78598932+down-shift@users.noreply.github.com>
2021-12-16 22:19:54 +0300

    Add files via upload

---
+++ eval.py
@@ -0,0 +1,151 @@
+import numpy as np
+import cv2
+from PIL import Image, ImageEnhance
+from matplotlib import cm
+import matplotlib.pyplot as plt
+
+
+def one_hot_encode(label):
+
+    """ Функция осуществляет перекодировку текстового "названия" сигнала
+     в список элементов, соответствующий выходному сигналу
+
+     Входные параметры: текстовая метка
+     Выходные параметры: метка ввиде списка
+
+     Пример:
+        one_hot_encode("red") должно возвращать:        [1, 0, 0, 0, 0]
+        one_hot_encode("yellow") должно возвращать:     [0, 1, 0, 0, 0]
+        one_hot_encode("green") должно возвращать:      [0, 0, 1, 0, 0]
+        one_hot_encode("yellow_red") должно возвращать: [0, 0, 0, 1, 0]
+        one_hot_encode("off") должно возвращать:        [0, 0, 0, 0, 1]
+
+     """
+    one_hot_encoded = []
+
+    if label == "red":
+        one_hot_encoded = [1, 0, 0, 0, 0]
+    elif label == "yellow":
+        one_hot_encoded = [0, 1, 0, 0, 0]
+    elif label == "green":
+        one_hot_encoded = [0, 0, 1, 0, 0]
+    elif label == "yellow_red":
+        one_hot_encoded = [0, 0, 0, 1, 0]
+    elif label == "off":
+        one_hot_encoded = [0, 0, 0, 0, 1]
+
+    return one_hot_encoded
+
+
+def increase_contrast(img):
+    pil_img = Image.fromarray((img * 255).astype(np.uint8))
+    enhancer = ImageEnhance.Contrast(pil_img)
+    output_img = enhancer.enhance(factor=1.25)
+    output_arr = cv2.bitwise_not(np.array(output_img))
+    return output_arr
+
+
+def detect_contours(img):
+    img_copy = cv2.cvtColor(img.copy(), cv2.COLOR_GRAY2BGR)
+    img_canny = cv2.Canny(img, 150, 200)
+    kernel = cv2.getStructuringElement(shape=cv2.MORPH_RECT, ksize=(5, 5))
+    img_closed_conts = cv2.morphologyEx(img_canny, cv2.MORPH_CLOSE, kernel)
+    circles = cv2.HoughCircles(img_closed_conts, cv2.HOUGH_GRADIENT, 1, 20, 50, 30)
+    for i in circles:
+        # draw the outer circle
+        print(i)
+        cv2.circle(img_copy,(i[0],i[1]),i[2],(0,255,0),2)
+    #contours, _ = cv2.findContours(img_closed_conts, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
+    #img_copy = cv2.cvtColor(img_copy, cv2.COLOR_GRAY2BGR)
+    #cv2.drawContours(img_copy, contours, contourIdx=-1, color=(255, 255, 0), thickness=2)
+    cv2.imshow('closed_contours', img_copy)
+    while cv2.waitKey(1) != ord('n'):
+        continue
+    #return contours
+
+
+def standardize_input(image):
+    """Приведение изображений к стандартному виду. 
+    Входные данные: изображение (bgr)
+    Выходные данные: стандартизированное изображений.
+    """
+
+    image = increase_contrast(image)
+    if len(image.shape) >= 3:
+        hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        val = cv2.resize(hsv_img[:, :, 2], (30, 90))
+    else:
+        val = image
+    #val = hsv_img[:, :, 2]
+    cutoff_x, cutoff_y = 6, 0
+    val = val[cutoff_x:90 - cutoff_x, cutoff_y:30 - cutoff_y]
+
+    contours = detect_contours(val)
+
+    val = cv2.GaussianBlur(val, (5, 5), sigmaX=6.0)
+
+    #cv2.imshow('frame', val)
+    #while cv2.waitKey(1) != ord('q'):
+    #    continue
+
+    ## TODO: Если вы хотите преобразовать изображение в формат, одинаковый для всех изображений, сделайте это здесь.
+    return val
+
+
+def predict_label(rgb_image):
+    """
+     функция определения сигнала светофора по входному изображению
+
+     Входные данные: изображение (bgr)
+     Выходные данные: метка в формате списка (смотри one_hot_encode)
+
+    """
+
+    #img = cv2.imread(file_path)
+    #print(rgb_image.shape)
+    #cv2.imshow('wth', rgb_image)
+    #while True:
+    #    if cv2.waitKey(1) == ord('q'):
+    #        break
+    val = standardize_input(rgb_image)
+    
+    #cv2.imshow('val', val)
+    h, w = val.shape
+    light_h, light_w = h // 3, w
+    red_sum = np.sum(val[:light_h, :])
+    yellow_sum = np.sum(val[light_h + 1:2 * light_h, :])
+    green_sum = np.sum(val[2 * light_h + 1:, :])
+    #color_sums.append([red_sum, yellow_sum, green_sum])
+    #print(red_sum, yellow_sum, green_sum)
+
+    dark_thr = 27000    # image darkness threshold
+    difference_thr = 19000   # max color difference
+
+    result = ''
+
+    if abs(red_sum - yellow_sum) < difference_thr and abs(yellow_sum - green_sum) < difference_thr and \
+        abs(red_sum - green_sum) < difference_thr:
+        result = 'off'
+        #print('hi')
+    else:
+        if red_sum > dark_thr and yellow_sum > dark_thr and green_sum > dark_thr:
+            if red_sum > yellow_sum and red_sum > green_sum:
+                if abs(red_sum - yellow_sum) < difference_thr and yellow_sum > green_sum:
+                    result = 'yellow_red'
+                else:
+                    result = 'red'
+            elif yellow_sum > red_sum and yellow_sum > green_sum:
+                if abs(red_sum - yellow_sum) < difference_thr and red_sum > green_sum:
+                    result = 'yellow_red'
+                else:
+                    result = 'yellow'
+            elif green_sum > red_sum and green_sum > yellow_sum:
+                result = 'green'
+        else:
+            result = 'off'
+            #print('hi')
+    
+    encoded_label = one_hot_encode(result)  # по умолчанию, говорит что на всех изображения жёлтый сигнал
+
+    ## TODO: ваша функция распознавания сигнала светофора должна быть здесь.
+    return encoded_label
---
+++ main.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+# Файл служит для определения точности вашего алгоритма
+# и не предназначен для редактирования участниками олимпиады
+# Для получения оценки точности, запустите файл на исполнение
+
+import cv2  # computer vision library
+import helpers  # helper functions
+import random
+import eval
+
+
+# Image data directories
+def load_data():
+    IMAGE_DIR_TRAINING = "traffic_light_images/training/"
+    #IMAGE_DIR_VALIDATION = "traffic_light_images/val/"
+    TRAINING_IMAGE_LIST = helpers.load_dataset(IMAGE_DIR_TRAINING)
+    #VALIDATION_IMAGE_LIST = helpers.load_dataset(IMAGE_DIR_VALIDATION)
+    #return TRAINING_IMAGE_LIST, VALIDATION_IMAGE_LIST
+    return TRAINING_IMAGE_LIST, []
+
+
+# приведение всего набора изображений к стандартному виду
+def standardize(image_list):
+    """Функция осуществляет приведение всего набора изображений к стндартному виду, а метки перводит в формат списка
+    Входные данные: список изображений
+    Выходные данные: стандартизированный список [изображение, метка]
+    """
+
+    # Empty image data array
+    standard_list = []
+    # Iterate through all the image-label pairs
+    for item in image_list:
+        image = item[0]
+        label = item[1]
+        # Standardize the image
+        standardized_im = eval.standardize_input(image)
+
+        # One-hot encode the label
+        one_hot_label = eval.one_hot_encode(label)
+
+        # Append the image, and it's one hot encoded label to the full, processed list of image data
+        standard_list.append((standardized_im, one_hot_label))
+
+    return standard_list
+
+
+# Constructs a list of misclassified images given a list of test images and their labels
+# This will throw an AssertionError if labels are not standardized (one-hot encoded)
+
+def get_misclassified_images(test_images):
+    """Определение точности работа алгоритма
+    Сравниваются результаты классификации вашего алгоритма и истинныме метки
+
+    Входные данные: массив с тестовыми изображениями и метками к ним
+    Выходные данные: массив с неправильно классифицированными метками
+
+    Этот код используется для тестирования и не должен изменяться
+    """
+
+    misclassified_images_labels = []
+
+    for image in test_images:
+
+        im = image[0]
+        true_label = image[1]
+        assert (len(true_label) == 5), "Метка имеет не верную длинну (нужно 5 значений)."
+
+        predicted_label = eval.predict_label(im)
+        assert (len(predicted_label) == 5), "Метка имеет не верную длинну (неужно 5 значений)."
+
+        if predicted_label != true_label:
+            misclassified_images_labels.append((im, predicted_label, true_label))
+
+    return misclassified_images_labels
+
+
+def main():
+    TRAIN_IMAGE_LIST, VALIDATION_IMAGE_LIST = load_data()
+    # Standardize the test data
+    STANDARDIZED_TRAIN_LIST = standardize(TRAIN_IMAGE_LIST)
+    #STANDARDIZED_VAL_LIST = standardize(VALIDATION_IMAGE_LIST)
+
+    # Shuffle the standardized test data
+    random.shuffle(STANDARDIZED_TRAIN_LIST)
+    #random.shuffle(STANDARDIZED_VAL_LIST)
+
+    # Find all misclassified images in a given test set
+    MISCLASSIFIED = get_misclassified_images(STANDARDIZED_TRAIN_LIST)
+
+    # Accuracy calculations
+    total = len(STANDARDIZED_TRAIN_LIST)
+    num_correct = total - len(MISCLASSIFIED)
+    accuracy = num_correct / total
+
+    print('Accuracy: ' + str(accuracy))
+    print("Number of misclassified images = " + str(len(MISCLASSIFIED)) + ' out of ' + str(total))
+    print(cv2.useOptimized())
+    
+
+def show_data():
+    data, _ = load_data()
+    data = standardize(data)
+    n = 0
+    print(data[0])
+    while cv2.waitKey(1) != ord('q'):
+        cv2.imshow(str(n), data[n][0])
+        if cv2.waitKey(1) == ord('n'):
+            n += 1
+
+if __name__ == '__main__':
+    main()
